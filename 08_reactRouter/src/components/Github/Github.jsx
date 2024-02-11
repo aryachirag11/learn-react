5753200925faee415df271b0183a491d57bc94d9
aryachirag11 <aryachirag11@gmail.com>
2024-02-11 15:44:30 +0530

    reactRouter done

---
+++ 08_reactRouter/src/components/Github/Github.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { useState } from "react";
+import { useEffect } from "react";
+import { useLoaderData } from "react-router-dom";
+
+const Github = () => {
+  const data = useLoaderData();
+  // // const [data, setData] = useState([]);
+  // // useEffect(() => {
+  // //   fetch("https://api.github.com/users/aryachirag11")
+  // //     .then((response) => response.json())
+  // //     .then((data) => {
+  // //       console.log(data);
+  // //       setData(data);
+  // //     });
+  // });
+  return (
+    <div className="text-center m-4 bg-gray-500 text-white p-4 text-3xl">
+      Github : {data.bio}
+      <img src="{data.avatar_url}" alt="" width={300} />
+    </div>
+  );
+};
+
+export default Github;
+
+export const githubInfoLoader = async () => {
+  const response = await fetch("https://api.github.com/users/aryachirag11");
+  return response.json();
+};
