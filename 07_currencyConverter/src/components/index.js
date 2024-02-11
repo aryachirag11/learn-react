c516eb56c28779aca417fee202afe8a1f569934d
aryachirag11 <aryachirag11@gmail.com>
2024-02-11 11:58:17 +0530

    #07 custom hooks and currencyConverter completed

---
+++ 07_currencyConverter/src/components/index.js
@@ -0,0 +1,5 @@
+// to improve code readability and a little bit of optimization when working with a big project and handling large number of components, always import all the components in a single file and then export them form that file
+
+import Input from "./Input";
+
+export { Input };
