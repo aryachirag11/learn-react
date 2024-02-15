8008fb5f31ed5ca85d40cd13428b841e3ebc2e6a
aryachirag11 <aryachirag11@gmail.com>
2024-02-15 11:36:07 +0530

    #7 created : components-Header,Footer,Button,Input and LogoutBtn

---
+++ 12_Blogger/src/components/container/container.jsx
@@ -0,0 +1,7 @@
+import React, { Children } from "react";
+
+const container = ({ children }) => {
+  return <div className="w-full max-w-7xl mx-auto px-4">{children}</div>;
+};
+
+export default container;
--- 12_Blogger/src/components/index.js
+++ 12_Blogger/src/components/index.js
@@ -1,4 +1,8 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
+import Container from "./container/container";
+import Logo from "./Logo";
+import LogoutBtn from "./Header/LogoutBtn";
+import Button from "./Button";
 
-export { Header, Footer };
+export { Header, Footer, Container, Logo, LogoutBtn, Button };
